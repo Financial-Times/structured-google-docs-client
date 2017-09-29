1d8af553997026c05e32faea1a5a130615e02b9b
Joanna Kao <joanna.kao@ft.com>
2017-09-29 16:41:08 -0400

    add code to transfer anything inside  into markup

--- src/transform-doc.js
+++ src/transform-doc.js
@@ -4,11 +4,23 @@ import stringReplaceAsync from 'string-replace-async';
 export default async function parseDoc(doc, options) {
   const $ = cheerio.load(doc);
 
+  // render markup (anything surrounded by ``` markup here ```) as HTML
+  const markupRegex = /`{3}(.+?)`{3}/g;
+  const withMarkupHTML = await stringReplaceAsync($('body').html(), markupRegex, async (match, markup) => markup
+    .replace(/<p>/g, '')
+    .replace(/<\/p>/g, '')
+    .replace(/&lt;/g, '<')
+    .replace(/&gt;/g, '>')
+    .replace(/&quot;/g, '\'')
+    .replace(/&#x201C;/g, '\'')
+    .replace(/&#x201D;/g, '\''));
+  $('body').html(withMarkupHTML);
+
+  // process annotations
   if (options && options.transform) {
-    // process annotations
     const annotationRegex = /\[{3}(.+?)\]{3}\s*\[{2}(.+?)(\|(.+?))?\]{2}/g;
-    const newBodyHTML = await stringReplaceAsync($('body').html(), annotationRegex, async (match, contentReference, annotation, x, label) => options.transform(contentReference, annotation, label));
-    $('body').html(newBodyHTML);
+    const transformedHTML = await stringReplaceAsync($('body').html(), annotationRegex, async (match, contentReference, annotation, x, label) => options.transform(contentReference, annotation, label));
+    $('body').html(transformedHTML);
   }
 
   return $.html();
