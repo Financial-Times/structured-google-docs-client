eefa4c85e2188ef75148ed7190b2f8c93714723a
Ændrew Rininsland <andrew.rininsland@ft.com>
2018-06-19 12:49:21 +0100

    :sparkles: Adds middleware ability. Fixes #4.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import cheerio from 'cheerio';
 import getRawGoogleDoc from './get-raw-google-doc';
 import sanitizeDoc from './sanitize-doc';
 import transformDoc from './transform-doc';
+import middlewareDoc from './process-middleware';
 
-export default async (googleDocId, options) => {
+export default async (googleDocId, options, middleware = []) => {
   const rawGoogleDoc = await getRawGoogleDoc(googleDocId);
   const $ = cheerio.load(rawGoogleDoc);
 
   const sanitizedDoc = await sanitizeDoc($('body').html());
-  return transformDoc(sanitizedDoc, options);
+  const transformedDoc = transformDoc(sanitizedDoc, options);
+
+  return middlewareDoc(middleware, transformedDoc);
 };
